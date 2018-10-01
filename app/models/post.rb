43bdecfdaf02ff540262ccacbcc44865f6fdefe2
daltobon <daniel18.tobon@gmail.com>
2018-10-01 15:59:31 -0500

    post storage

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,14 +1,15 @@
 class Post < ApplicationRecord
   belongs_to :user
 
-  has_attached_file :image, styles: {large: "1024x768", medium: "720x240", thumb: "640x480>" }
+  has_attached_file :image, styles: {large: "1024x768", medium: "720x240", thumb: "640x480>" },
   :storage => :s3,
-    :s3_credentials => {
+  :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
       :region => ENV['AWS_REGION']
     },
+    
     :path => ":filename.:extension",
     # :path => ":rails_root/public/system/:attachment/:id/:style/:filename", 
     :default_url => "default_img.png"

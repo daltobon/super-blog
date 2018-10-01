470d558190e79d048ac5d1b698a4ab252704385c
daltobon <daniel18.tobon@gmail.com>
2018-10-01 17:44:03 -0500

    Protocol https

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,6 +4,7 @@ class Post < ApplicationRecord
   has_attached_file :image, styles: {large: "1024x768", medium: "720x240", thumb: "640x480>" },
    :convert_options => {
     :thumb => "-quality 75 -strip" },
+    :s3_protocol => :https,
     :storage => :s3,
   :s3_credentials => {
       :bucket => ENV['S3_BUCKET_NAME'],
@@ -11,7 +12,7 @@ class Post < ApplicationRecord
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
       :region => ENV['AWS_REGION']
     }
-    
+
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
   validates :title, presence: true
   validates :content, presence: true

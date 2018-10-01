a49b19cc2fa8091a9fe4e48dcd33f634e2ef0590
daltobon <daniel18.tobon@gmail.com>
2018-10-01 14:46:16 -0500

    Add server AWS3

--- Gemfile
+++ Gemfile
@@ -17,6 +17,7 @@ gem 'better_errors', '~> 2.4'
 gem 'bulma-rails', '~> 0.6.1'
 gem 'simple_form', '~> 4.0', '>= 4.0.1'
 gem "paperclip", "~> 6.0.0"
+gem 'aws-sdk', '~> 2.3'
 
 group :development, :test do
  
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,15 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  config.paperclip_defaults = {
+  storage: :s3,
+  s3_credentials: {
+    bucket: ENV.fetch('danielrails'),
+    access_key_id: ENV.fetch('AKIAIPQ6JQE6ZWK2ISVQ'),
+    secret_access_key: ENV.fetch('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'),
+    s3_region: ENV.fetch('sa-east-1'),
+  }
+}
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.

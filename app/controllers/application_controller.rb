93ea62735b96a75904038b0161fe9357fe7a44c5
daltobon <daniel18.tobon@gmail.com>
2018-10-02 18:59:12 -0500

    user authentic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def authenticate_user!
-    unless current_user.present? && current_user.post?
+    unless current_user = user.post
       flash[:danger] = "No tienes autorización para entrar en esa sección"
       redirect_to root_path
     end

99a419b356bdbebcf06c99f16bdd0ffe07a63a53
daltobon <daniel18.tobon@gmail.com>
2018-10-10 14:01:20 -0500

    email confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
+  after_create  :send_confirmation_instructions, :if => :confirmation_required?
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable, :timeoutable
 
   has_many :posts
   has_many :comments
+
 end

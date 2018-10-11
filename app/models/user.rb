4c8ac960acbc19644c588c1056e814fa5abda5c6
Daniel Tobón <daniel18.tobon@gmail.com>
2018-10-10 22:12:47 -0500

    email only once

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ApplicationRecord
-  after_create  :send_confirmation_instructions, :if => :confirmation_required?
-  # Include default devise modules. Others available are:
+    # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :validatable, :timeoutable
 
   has_many :posts
   has_many :comments
+
+  def confirm!
+  super
+  account_created
+end
 
 end
